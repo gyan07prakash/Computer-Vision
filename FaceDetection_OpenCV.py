a42090ba0fa5a81f1de1bfb945f513588595f9b6
Gyan Prakash <gyan07prakash@gmail.com>
2025-05-24 19:49:36 +0530

    Create FaceDetection_OpenCV.py
    A simple real-time face detection application using OpenCV in Python.
    This project uses the Haar Cascade classifier to detect human faces from a webcam feed, drawing bounding boxes and labels over detected faces.
    Useful for basic computer vision tasks, surveillance projects, and learning OpenCV's face detection APIs.

---
+++ FaceDetection_OpenCV.py
@@ -0,0 +1,17 @@
+import cv2
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 4)
+    cv2.imshow('Image', frame)
+
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
